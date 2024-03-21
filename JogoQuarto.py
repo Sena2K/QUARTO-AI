d5c911724cde670023d70ff06c85c5d0fc89d515
Erik Zaina <erik.zaina2@gmail.com>
2024-03-21 00:41:14 -0300

    agora ta jogando com a IA mas tem bug em algumas jogadas tem q dar o fix

--- JogoQuarto.py
+++ JogoQuarto.py
@@ -1,3 +1,4 @@
+import sys
 import random
 
 
@@ -27,11 +28,11 @@ class JogoQuarto:
             for j in range(4):
                 peca = self.tabuleiro[i][j]
                 if peca is None:
-                    print(".", end=" ")
+                    print("\033[30m.\033[m", end=" ")  # Representa peça vazia com ponto preto
                 else:
-                    print("X" if peca == 1 else "O", end=" ")
+                    cor = "\033[31m" if peca == 1 else "\033[34m"  # Cor vermelha para jogador 1, azul para jogador 2
+                    print(cor + "X" + "\033[m", end=" ")  # X representa uma peça ocupada
             print()
-        print()
 
     def jogador_venceu(self, jogador):
         for posicoes in self.posicoes_vencedoras:
@@ -40,7 +41,7 @@ class JogoQuarto:
         return False
 
     def empate(self):
-        return all(self.tabuleiro[i][j] is not None for i in range(4) for j in range(4) if self.tabuleiro[i][j] is not None)
+        return all(self.tabuleiro[i][j] is not None for i in range(4) for j in range(4))
 
     def obter_jogadas_validas(self):
         return [(i, j, *self.pecas_disponiveis[-1]) for i in range(4) for j in range(4) if self.tabuleiro[i][j] is None]
@@ -52,6 +53,7 @@ class JogoQuarto:
 
         self.tabuleiro[i][j] = self.jogador_atual
         self.pecas_disponiveis.pop()
+
         if self.jogador_venceu(self.jogador_atual):
             return self.jogador_atual
         elif self.empate():
@@ -60,23 +62,88 @@ class JogoQuarto:
             self.jogador_atual = 3 - self.jogador_atual  # Trocar de jogador
             return None
 
+    def jogar(self):
+        while True:
+            self.exibir_tabuleiro()
+            jogadas_validas = self.obter_jogadas_validas()
+            print("Peça disponível:", self.pecas_disponiveis[-1])
+            print("Jogadas válidas:", jogadas_validas)
+            if jogadas_validas:
+                jogada = None
+                while jogada not in jogadas_validas:
+                    jogada = input("Digite sua jogada (linha coluna cor altura forma consistencia): ")
+                    try:
+                        jogada = tuple(map(int, jogada.split()))
+                        if len(jogada) != 6:
+                            raise ValueError
+                    except (ValueError, IndexError):
+                        print("Entrada inválida. Por favor, digite apenas os valores separados por espaços.")
+                        jogada = None
+                resultado = self.fazer_jogada(jogada)
+                if resultado is not None:
+                    if resultado == 0:
+                        print("Empate!")
+                    elif resultado == "IA":
+                        print("IA ganhou!")
+                    else:
+                        print("Jogador", resultado, "venceu!")
+                    break
+
 
 class IAQuarto:
     def __init__(self, profundidade):
         self.profundidade = profundidade
 
     def avaliar(self, jogo):
+        #HEURISTICA
         linhas_jogador1 = sum(
             1 for posicoes in jogo.posicoes_vencedoras if all(jogo.tabuleiro[i][j] == 1 for i, j in posicoes))
         linhas_jogador2 = sum(
             1 for posicoes in jogo.posicoes_vencedoras if all(jogo.tabuleiro[i][j] == 2 for i, j in posicoes))
         return linhas_jogador1 - linhas_jogador2
 
+    def minimax(self, jogo, profundidade, jogador_maximizador, alfa, beta):
+        if profundidade == 0 or jogo.jogador_venceu(1) or jogo.jogador_venceu(2) or jogo.empate():
+            return self.avaliar(jogo), None
+
+        if jogador_maximizador:
+            max_eval = -sys.maxsize
+            melhor_jogada = None
+            for jogada in jogo.obter_jogadas_validas():
+                jogo.fazer_jogada(jogada)
+                eval, _ = self.minimax(jogo, profundidade - 1, False, alfa, beta)
+                jogo.tabuleiro[jogada[0]][jogada[1]] = None
+                jogo.pecas_disponiveis.append(jogada[2:])
+                if eval > max_eval:
+                    max_eval = eval
+                    melhor_jogada = jogada
+                alfa = max(alfa, eval)
+                if beta <= alfa:
+                    break
+            return max_eval, melhor_jogada
+        else:
+            min_eval = sys.maxsize
+            melhor_jogada = None
+            for jogada in jogo.obter_jogadas_validas():
+                jogo.fazer_jogada(jogada)
+                eval, _ = self.minimax(jogo, profundidade - 1, True, alfa, beta)
+                jogo.tabuleiro[jogada[0]][jogada[1]] = None
+                jogo.pecas_disponiveis.append(jogada[2:])
+                if eval < min_eval:
+                    min_eval = eval
+                    melhor_jogada = jogada
+                beta = min(beta, eval)
+                if beta <= alfa:
+                    break
+            return min_eval, melhor_jogada
+
+    def escolher_jogada(self, jogo):
+        return random.choice(jogo.obter_jogadas_validas())
+
 
 if __name__ == "__main__":
     jogo = JogoQuarto()
-    ia = IAQuarto(profundidade=3)
-
+    ia = IAQuarto(profundidade=3)  # Ajustar a profundidade conforme necessário
     while True:
         jogo.exibir_tabuleiro()
         jogadas_validas = jogo.obter_jogadas_validas()
@@ -100,6 +167,8 @@ if __name__ == "__main__":
             if resultado is not None:
                 if resultado == 0:
                     print("Empate!")
+                elif resultado == "IA":
+                    print("IA ganhou!")
                 else:
                     print("Jogador", resultado, "venceu!")
                 break
